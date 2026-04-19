7a13ae15de732b4168277eabc361933d3d7ff91d
Leonard de Ruijter <alderuijter@gmail.com>
2026-04-19 13:47:34 +0200

    Update

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("Resources for studying Biblical ancient languages for Dutch users"),
 	# version
-	"addon_version": "2025.2",
+	"addon_version": "2026.1",
 	# Author(s)
 	"addon_author": "Leonard de Ruijter <alderuijter@gmail.com>",
 	# URL for the add-on documentation support
